2ae7fff84f9102e93b9402665aafa5c5f99b56f5
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2020-07-31 23:22:53 +0200

    Output PNG files instead of PPM

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::error::Error;
 use std::fs::File;
-use std::io::{prelude::*, BufWriter};
+use std::io::BufWriter;
 use std::path::Path;
 
 mod hit;
@@ -35,7 +35,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Output file
     let args: Vec<String> = env::args().collect();
     let path = Path::new(&args[1]);
-    let mut file = BufWriter::new(File::create(&path).unwrap());
+    let file = BufWriter::new(File::create(&path).unwrap());
+    let mut encoder = png::Encoder::new(file, IMAGE_WIDTH, IMAGE_HEIGHT);
+    encoder.set_color(png::ColorType::RGB);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header()?;
+
+    let mut pixels = Vec::<u8>::with_capacity((IMAGE_WIDTH * IMAGE_HEIGHT * 4) as usize);
 
     // Camera
     let viewport_height = 2.0;
@@ -47,8 +53,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let vertical = Vec3(0.0, viewport_height, 0.0);
     let lower_left_corner =
         origin - horizontal / 2.0 - vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
-
-    writeln!(&mut file, "P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT)?;
 
     for j in (0..IMAGE_HEIGHT).rev() {
         println!("Scanline {}/{}", IMAGE_HEIGHT - j, IMAGE_HEIGHT);
@@ -62,21 +66,19 @@ fn main() -> Result<(), Box<dyn Error>> {
             );
             let pixel_color = ray_color(ray);
 
-            write_color(&mut file, pixel_color)?;
+            write_color(&mut pixels, pixel_color);
         }
     }
 
-    file.flush()?;
+    writer.write_image_data(&pixels)?;
 
     println!("Done.");
     Ok(())
 }
 
-fn write_color<F: Write>(f: &mut F, color: Vec3) -> std::io::Result<()> {
+fn write_color(pixels: &mut Vec<u8>, color: Vec3) {
     let color = 255.999 * color; // Map from [0-1] to [0-255]
-    writeln!(
-        f,
-        "{} {} {}",
-        color.0 as u32, color.1 as u32, color.2 as u32
-    )
+    pixels.push(color.0 as u8);
+    pixels.push(color.1 as u8);
+    pixels.push(color.2 as u8);
 }
