a2440898cf57c15bdd9730778e603b9290c09aa6
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2020-08-25 22:33:24 +0200

    Add two more bench scenes

--- benches/basic.rs
+++ benches/basic.rs
@@ -7,18 +7,44 @@ fn load_scene(path: &str) -> Scene {
     deserialize_scene(&file_content).unwrap()
 }
 
+const SPHERE_IMAGE_SETTINGS: ImageSettings = ImageSettings {
+    width: 100,
+    height: 100,
+};
+const SPHERE_RENDER_SETTINGS: RenderSettings = RenderSettings {
+    samples_per_pixel: 5,
+    max_depth: 10,
+    thread_count: 1,
+};
+
 fn sphere1() {
     let scene = load_scene("benches/sphere1.scene");
-    let image_settings = ImageSettings {
-        width: 100,
-        height: 100,
-    };
-    let render_settings = RenderSettings {
-        samples_per_pixel: 5,
-        max_depth: 10,
-        thread_count: 1,
-    };
-    render(&scene, &image_settings, &render_settings, false);
+    render(
+        &scene,
+        &SPHERE_IMAGE_SETTINGS,
+        &SPHERE_RENDER_SETTINGS,
+        false,
+    );
+}
+
+fn sphere2() {
+    let scene = load_scene("benches/sphere2.scene");
+    render(
+        &scene,
+        &SPHERE_IMAGE_SETTINGS,
+        &SPHERE_RENDER_SETTINGS,
+        false,
+    );
+}
+
+fn sphere3() {
+    let scene = load_scene("benches/sphere3.scene");
+    render(
+        &scene,
+        &SPHERE_IMAGE_SETTINGS,
+        &SPHERE_RENDER_SETTINGS,
+        false,
+    );
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -26,6 +52,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.sample_size(20);
     group.measurement_time(Duration::from_secs(25));
     group.bench_function("sphere1", |b| b.iter(sphere1));
+    group.bench_function("sphere2", |b| b.iter(sphere2));
+    group.bench_function("sphere3", |b| b.iter(sphere3));
     group.finish();
 }
 
